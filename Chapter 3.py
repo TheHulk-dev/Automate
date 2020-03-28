81cca5e41cf9d95c4ab683781eff80bfdfed16b1
The Hulk <TheHulk@local.co>
2020-03-28 12:38:20 -0500

    Added chapter 3.py

---
+++ Chapter 3.py
@@ -0,0 +1,22 @@
+def collatz(number):
+    if number % 2 == 0:
+        number /= 2
+    else:
+        number = number*3+1
+    return int(number)
+
+
+BonneEntree = False
+while not BonneEntree:
+    print('Entrez un nombre entier :')
+    try:
+        number = int(input())
+        BonneEntree = True
+
+    except ValueError as erreur:
+        # print(erreur)
+        print('Ce n\'est pas un nombre entier !')
+
+while number != 1:
+    number = collatz(number)
+    print(number)
